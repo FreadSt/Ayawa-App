b97c4f65e94be4805002bbd3dd303207a4e9fd4c
Vladimir Kholiavko <thiswolfram@gmail.com>
2023-02-10 14:31:29 +0300

    ended fix

--- src/components/reset/Reset.jsx
+++ src/components/reset/Reset.jsx
@@ -4,7 +4,15 @@ import videoBG from "../../assets/images/background.mp4";
 import image from "../../assets/images/reset.png";
 import tick from "../../assets/images/Tick.png";
 import hide from "../../assets/images/Hide.png";
+import succsess from "../../assets/images/subscribe.png";
 import {useState, useEffect} from 'react';
+import styled, {keyframes} from "styled-components";
+import {fadeInDownBig, fadeInUpBig} from "react-animations";
+const fadeAnimation = keyframes`${fadeInDownBig}`;
+
+const FadeDiv = styled.div`
+  animation: 1s ${fadeAnimation};
+`;
 
 const Reset = () => {
     const [passdata, setPassData] = useState({value: '', error: ''})
@@ -16,6 +24,18 @@ const Reset = () => {
         if(!passdata.value) {
             setPassData({value: "", error:"Password is required"})
             isError = true
+        }
+        if(passdata.value.length < 6) {
+            isError = true;
+            setPassData({value: "", error:"Password is required"})
+        }
+        if(!/\d/.test(passdata.value)){
+            isError = true
+            setPassData({value: "", error:"Password is required"})
+        }
+        if(!/[A-Z]/.test(passdata.value)){
+            isError = true;
+            setPassData({value: "", error:"Password is required"})
         }
         if(!isError){
             setPassData({value:"", error:""})
@@ -34,20 +54,22 @@ const Reset = () => {
     return(
         <div className={'reset'}>
             {
-                !isSubmit?
-                    <div className={'submit-window'}>
-                        <video src={videoBG} autoPlay muted loop/>
-                        <div className={'inner-container'}>
-                            <img src={image} alt={''} className={'main-img'}/>
-                            <div className={'text'}>
-                                <h1>Your new password
-                                    has been accepted</h1>
-                                <p>You no longer need this page.
-                                    You may close it and return back to the app.</p>
-                                <span>Good luck!</span>
+                isSubmit?
+                    <FadeDiv>
+                        <div className={'submit-window'}>
+                            <video src={videoBG} autoPlay muted loop/>
+                            <div className={'inner-container'}>
+                                <img src={succsess} alt={''} className={'succsess-img'}/>
+                                <div className={'text'}>
+                                    <h1>Your new password
+                                        has been accepted</h1>
+                                    <p>You no longer need this page.
+                                        You may close it and return back to the app.</p>
+                                    <span>Good luck!</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </FadeDiv>
                     :
                     <div className={'window'}>
                         <video src={videoBG} autoPlay muted loop/>
